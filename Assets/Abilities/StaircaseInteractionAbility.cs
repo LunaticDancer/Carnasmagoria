10300039915f7d9a71cc8ae17befb0c4d5206be7
lunaticdancergames@gmail.com <lunaticdancergames@gmail.com>
2022-12-23 16:28:23 +0100

    basic set up for the staircase system, some loose ends

---
+++ Assets/Abilities/StaircaseInteractionAbility.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewAbility", menuName = "Scriptables/Abilities/StaircaseInteractionAbility")]
+public class StaircaseInteractionAbility : Ability
+{
+	[SerializeField] private LevelData targetLevel = null;
+
+	[HideInInspector] public LevelData TargetLevel { get => targetLevel; }
+
+	public override bool Cast(Creature caster, Tile target)
+	{
+		if (targetLevel)
+		{
+			GameController.Instance.WorldController.LevelGenerator.GenerateLevelLayout(targetLevel);
+			return true;
+		}
+		return false;
+	}
+}
